387c52e54ccb168ba415d497b333ba36fe308dfb
Om Thanage <om.thanage@somaiya.edu>
2024-08-10 00:31:06 +0530

    Change style of event and project details project Change file format of project images to webp

--- src/containers/projects/ProjectsDetails.jsx
+++ src/containers/projects/ProjectsDetails.jsx
@@ -3,7 +3,7 @@ const Projects = [
     id: 1,
     title: 'STUDENT SAT',
     author: 'SSRP',
-    src: "img10.jpg",
+    src: "img10.webp",
     url: '/project/{id}',
     description: 'Our inaugural StudentSat, guided by ISRO and leading space startups, aims to elevate educational opportunities and expand the horizons of space research. Join us as we inspire future engineers and students to turn their dreams into reality ',
     objective: 'Data collection, communications and conducting experiments.',
@@ -14,7 +14,7 @@ const Projects = [
     id:2,
     title: 'GROUND STATION',
     author: 'SSRP',
-    src: "img2.png",
+    src: "img2.webp",
     url: '/project/{id}',
     description: 'The ground station is a device which will be built and used for the purpose of satellite communication, data acquisition, and space research endeavors and to connect with the ham community(Amateur Radio community).',
     objective: 'To receive and transmit data to satellites and for tracking our satellite. It will be used to remotely interact with the ham community (Amateur Radio community).',
@@ -23,21 +23,22 @@ const Projects = [
   },
   {
     id:3,
-    title: 'CANSAT',
+    title: 'BALLOON SAT',
     author: 'SSRP',
-    src: "img3.png",
-    url: '/project/{id}',
-    description: 'The CanSat competition allows students to design, build, and launch miniature satellites resembling canisters. It provides practical experience in space science, progressing through design, construction, testing, and launch stages. CanSats are deployed via rockets to gather data during descent, with teams evaluated on design, functionality, data accuracy, and presentation.',
-    objective: 'Components: Container, Probe Altitude Range: 670m - 725m  Deployment: Peak altitude Orientation: Uncontrolled Survivability: Launch and deployment forces Objective: Collect and transmit data from peak altitude.',
     color: "black",
-    outcomes: 'Students learn to develop optimized systems under extreme space constraints, including mechanical space-grade systems that endure forces and vibrations.'
+    src: "img6.webp",
+    url: '/project/{id}',
+    description: 'Experiment involves a meteorological balloon ascending to 50 km above sea level. Equipped with a UV sensor, it measures UV levels across regions to assess ozone levels.',
+    objective: 'To gather ozone level data, attach a camera for topographic views, and establish ground station communication.',
+    outcomes: "Explores satellite propulsion methods while collecting data in Earth's atmosphere."
   },
+
   {
     id:4,
     title: 'SPACEPORT AMERICA',
     color: "black",
     author: 'SSRP',
-    src: "img4.png",
+    src: "img4.webp",
     url: '/project/{id}',
     description: "Intercollegiate rocket engineering competition by Spaceport America. Students design propulsion systems and avionics. Also aids in rocket recovery mechanism development for SSRP's CanSat testing.",
     objective: 'IREC teams design, build, and launch sounding rockets with payloads of minimum 8.8 lbs to reach apogees of 10,000 ft or 30,000 ft AGL. Aiming to advance research in monopropellant/hybrid rocket propulsion engines.',
@@ -48,7 +49,7 @@ const Projects = [
     title: 'INTERNATIONAL ROVER CHALLENGE',
     author: 'SSRP',
     color: "black",
-    src: "img5.avif",
+    src: "img5.webp",
     url: '/project/{id}',
     description: 'To conceptualize, design, develop and operate an extraterrestrial rover and perform specific missions in simulated conditions.',
     objective: 'Design and build a rover with autonomous navigation, featuring detachable modules including an arm and a laboratory module for detecting signs of ancient life. Equipped with sensors like spectrometers and gas sensors, it can manipulate objects up to 1kg weight, enabling research in extraterrestrial terrain.',
@@ -56,14 +57,14 @@ const Projects = [
   },
   {
     id:6,
-    title: 'BALLOON SAT',
+    title: 'CANSAT',
     author: 'SSRP',
-    color: "black",
-    src: "img6.jpg",
+    src: "img3.webp",
     url: '/project/{id}',
-    description: 'Experiment involves a meteorological balloon ascending to 50 km above sea level. Equipped with a UV sensor, it measures UV levels across regions to assess ozone levels.',
-    objective: 'To gather ozone level data, attach a camera for topographic views, and establish ground station communication.',
-    outcomes: "Explores satellite propulsion methods while collecting data in Earth's atmosphere."
+    description: 'The CanSat competition allows students to design, build, and launch miniature satellites resembling canisters. It provides practical experience in space science, progressing through design, construction, testing, and launch stages. CanSats are deployed via rockets to gather data during descent, with teams evaluated on design, functionality, data accuracy, and presentation.',
+    objective: 'Components: Container, Probe Altitude Range: 670m - 725m  Deployment: Peak altitude Orientation: Uncontrolled Survivability: Launch and deployment forces Objective: Collect and transmit data from peak altitude.',
+    color: "black",
+    outcomes: 'Students learn to develop optimized systems under extreme space constraints, including mechanical space-grade systems that endure forces and vibrations.'
   },
   // Add more projects as needed
 ];
